19a66b30fd9e8e290195e9811c514da265cc4909
binniethepooh <64795543+binniethepooh@users.noreply.github.com>
2020-09-11 11:19:03 +0900

    #9.2

--- users/models.py
+++ users/models.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+
 class User(AbstractUser):
 
     """ Custom User Model """
@@ -26,7 +28,7 @@ class User(AbstractUser):
 
     CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_KRW, "KRW"))
 
-    avatar = models.ImageField(blank=True)
+    avatar = models.ImageField(upload_to="avatars", blank=True)
     gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
     bio = models.TextField(blank=True)
     birthdate = models.DateField(blank=True, null=True)
